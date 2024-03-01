55d51812490044dbf8698a21061b2d9cecdef8e9
menene <efmarroquinr@gmail.com>
2024-03-01 07:41:53 -0600

    ciclos for

--- Ejemplos/Ciclos1/dibujo.py
+++ Ejemplos/Ciclos1/dibujo.py
@@ -1,12 +1,19 @@
 import calculos
 
 #  POLIGONO
+# def poligono(lados, tortuga, largo):
+#     angulo = calculos.angulo(lados)
+
+#     x = 1
+#     while x <= lados:
+#         tortuga.forward(largo)
+#         tortuga.left(angulo)
+
+#         x = x + 1
+
 def poligono(lados, tortuga, largo):
     angulo = calculos.angulo(lados)
-
-    x = 1
-    while x <= lados:
+    
+    for i in range(lados):
         tortuga.forward(largo)
         tortuga.left(angulo)
-
-        x = x + 1
---
+++ Ejemplos/ciclo_for.py
@@ -0,0 +1,4 @@
+listado = range(10, 20, 5)
+
+for i in listado:
+	print(i)
