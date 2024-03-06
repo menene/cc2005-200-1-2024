fe35cb132902a10d18ef72fe0759e310bc1b55c5
menene <efmarroquinr@gmail.com>
2024-03-06 08:59:32 -0600

    encabezado para copy paste

---
+++ encabezado.py
@@ -0,0 +1,9 @@
+#==========================================================
+# Universidad del Valle de Guatemala
+# Algoritmos y progra basica
+# Seccion 60
+# 
+# Erick Marroquin - 123456
+# EJERCICIO
+# 06/03/24
+#==========================================================
